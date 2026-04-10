cfb02469c87f99b4952e9b8435ecae4dd432cebb
Mark <markimade01@gmail.com>
2026-04-10 14:05:51 +0000

    Implemented basic ECS model

--- joystick/Joystick.cpp
+++ joystick/Joystick.cpp
@@ -1,17 +1,19 @@
 #include "mbed.h"
 #include "Joystick.h"
 
-Joystick::Joystick(PinName analogX, PinName analogY) : analogX_(analogX), analogY_(analogY) {
-    printf("New object created");
-}
+Joystick::Joystick(PinName analogX, PinName analogY) : analogX_(analogX), analogY_(analogY) { };
 
-
-void Joystick::reportPosition() {
+array<float, 2> Joystick::reportPosition() {
 
     float posX{0}, posY{0};
-    printf("I was called");
+
     posX = analogX_.read();
     posY = analogY_.read();
-    printf("%.3f ", posX);
-    printf("%.3f \n", posY);
+    
+    array<float, 2> returnVal;
+    returnVal[0] = posX;
+    returnVal[1] = posY;
+
+    return returnVal;   
 }
+
